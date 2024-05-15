694e1c8040b95bcc142a0574d33d6fe3dd74289b
martins2986 <m.devit@pm.me>
2024-05-15 11:25:20 +0200

    created and connect to DB

--- api/server.js
+++ api/server.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
 const app = express();
-const PORT = 3000;
+const connectDB = async () => {
+  await mongoose.connect(process.env.URI);
+  console.log("DB Connected");
+};
+
+try {
+  connectDB();
+} catch (err) {
+  console.log(err);
+}
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
