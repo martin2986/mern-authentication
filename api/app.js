df9f0dd4ac4f0d1b5436e7e7685ec12c41d6bcc5
martins2986 <m.devit@pm.me>
2024-05-15 15:53:34 +0200

    created errorHandlers

--- api/app.js
+++ api/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import userRoute from "./routes/userRoute.js";
 import authRoute from "./routes/authRoute.js";
+import { errorHandler } from "./handlers/errorHandler.js";
 const app = express();
 app.use(express.json());
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
+app.use(errorHandler);
 export default app;
---
+++ api/handlers/errorHandler.js
@@ -0,0 +1,47 @@
+import AppError from "./AppError.js";
+
+//DEVELOPEMENT ERROR
+const developmentErrors = (error, req, res, next) => {
+  error.stack = error.stack || "";
+  const errorDetails = {
+    message: error.message,
+    status: error.status,
+    stackHighlighted: error.stack.replace(
+      /[a-z_-\d]+.js:\d+:\d+/gi,
+      "<mark>$&</mark>"
+    ),
+  };
+  return new AppError(error.message, 400);
+};
+const handleValidationError = (err) => {
+  const message = `Required fields are not supplied`;
+
+  return new AppError(message, 400);
+};
+const handleCastError = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+
+  return new AppError(message, 400);
+};
+export const errorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    developmentErrors(err, req, res, next);
+  } else {
+    res.status(err.statusCode).json({
+      success: false,
+      status: "error",
+      message: "something went wong",
+    });
+  }
+
+  if (err.name === "CastError") err = handleCastError(err);
+  if (err.name == "ValidationError") err = handleValidationError(err);
+  res.status(err.statusCode).json({
+    success: false,
+    status: err.status,
+    message: err.message,
+  });
+};
