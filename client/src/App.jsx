70aa1ae7e2d5ffd6e9b1c2911bc9edab048a47bf
martins2986 <m.devit@pm.me>
2024-05-15 11:04:21 +0200

    created navHeader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Error from "./pages/Error";
+import Header from "./components/Header";
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
